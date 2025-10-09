cf79274c286ff71744c15c6be7780148cb3cd9d0
DESKTOP-AB8J0V3\PC_User <y24340216@gmail.com>
2025-10-10 01:13:24 +0900

    フォルダと必須レベルのクラスの追加

---
+++ Jam/Scenes/GameScene.h
@@ -0,0 +1,31 @@
+﻿#pragma once
+#include <Siv3D.hpp>
+#include <HamFramework.hpp>
+#include "TitleScene.h"
+
+namespace Jam
+{
+	namespace Scenes
+	{
+		using App = SceneManager<String>;
+
+		class GameScene : public App::Scene
+		{
+
+		public:
+			GameScene(const InitData& init)
+				: IScene{ init }
+			{
+			}
+
+			void update() override
+			{
+			}
+
+			void draw() const override
+			{
+				Scene::SetBackground(ColorF{ 0.9, 0.9, 1.0 });
+			}
+		};
+	}
+}
---
+++ Jam/System/InputManager.h
@@ -0,0 +1,31 @@
+﻿#pragma once
+#include <Siv3D.hpp>
+
+namespace Jam
+{
+	namespace Game
+	{
+		class InputManager
+		{
+		public:
+			void update()
+			{
+				m_leftPressed = KeyA.pressed();
+				m_rightPressed = KeyD.pressed();
+				m_jumpTriggered = KeySpace.down() || KeyW.down();
+			}
+
+			// 横移動
+			bool left() const { return m_leftPressed; }
+			bool right() const { return m_rightPressed; }
+			// ジャンプ
+			bool jump() const { return m_jumpTriggered; }
+
+		private:
+			bool m_leftPressed = false;
+			bool m_rightPressed = false;
+			bool m_jumpTriggered = false;
+		};
+
+	}
+}
