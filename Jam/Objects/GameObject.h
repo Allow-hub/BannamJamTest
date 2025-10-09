45028447c9ab72511679ee3d3630c1d1689466d4
DESKTOP-AB8J0V3\PC_User <y24340216@gmail.com>
2025-10-10 01:19:19 +0900

    オブジェクトの基底クラスの追加

---
+++ Jam/Objects/GameObject.h
@@ -0,0 +1,31 @@
+﻿#pragma once
+#include <Siv3D.hpp>
+namespace Jam
+{
+	namespace Objects
+	{
+		class GameObject
+		{
+		protected:
+			Vec2 position;
+			bool active = true;
+			bool visible = true;
+
+		public:
+			virtual ~GameObject() = default;
+
+			virtual void update(double deltaTime) {}
+			virtual void draw() const {}
+
+			Vec2 getPos() const { return position; }
+			void setPos(const Vec2& pos) { position = pos; }
+
+			bool isActive() const { return active; }
+			void setActive(bool v) { active = v; }
+
+			bool isVisible() const { return visible; }
+			void setVisible(bool v) { visible = v; }
+		};
+
+	}
+}
