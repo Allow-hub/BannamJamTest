7f6a4f9592a2c118bf3e1ef5f469baa02d20ec8a
DESKTOP-AB8J0V3\PC_User <y24340216@gmail.com>
2025-10-23 03:48:52 +0900

    AttackのAI

---
+++ Jam/src/Domain/Enemy/EnemyAI/AttackAI.h
@@ -0,0 +1,26 @@
+﻿#pragma once
+#include "IEnemyAI.h"
+
+namespace Jam::Domain::Enemy
+{
+	struct AttackAI : IEnemyAI
+	{
+		void enter(EnemyBase& enemy) override
+		{
+			// フック
+			enemy.onAttackEnter();
+		}
+
+		void update(EnemyBase& enemy, double deltaTime) override
+		{
+			// フック
+			enemy.onAttackUpdate(deltaTime);
+		}
+
+		void exit(EnemyBase& enemy) override
+		{
+			// フック
+			enemy.onAttackExit();
+		}
+	};
+}
