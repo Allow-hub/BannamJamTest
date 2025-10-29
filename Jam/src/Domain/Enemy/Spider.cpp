06346168bc920bbf895b63d222b98a5ead9a5d6e
DESKTOP-AB8J0V3\PC_User <y24340216@gmail.com>
2025-10-30 00:51:27 +0900

    クモ調整

--- Jam/src/Domain/Enemy/Spider.cpp
+++ Jam/src/Domain/Enemy/Spider.cpp
@@ -150,11 +150,49 @@ namespace Jam::Domain::Enemy
 
 	void Spider::onAIEvent(EnemyAIEvent e)
 	{
+		switch (e)
+		{
+		case EnemyAIEvent::PlayerFound:
+			changeAI(AIType::Chase);
+			break;
+
+		case EnemyAIEvent::PlayerLost:
+			changeAI(AIType::Patrol);
+			break;
+		case EnemyAIEvent::ReachedGoal:
+			changeAI(AIType::Attack);
+			break;
+
+		default:
+			break;
+		}
 	}
 
 	void Spider::onCollisionEnter(std::shared_ptr<Jam::Domain::Physics::IPhysicsBody> other)
 	{
 		EnemyBase::onCollisionEnter(other);
+		switch (other->getLayer())
+		{
+		case Physics::PhysicsLayer::Player:
+			m_eventQueue.push(Events::PlayerDamagedEvent{
+				m_body->getID(),
+				m_playerId,
+				DamageInfo {
+				m_status.attackPower,
+				m_body->getPosition(),
+				(getPlayerPos() - m_body->getPosition()).normalized(),
+				true,
+				false
+				}
+				,0.0
+				,0.3
+				,15.0
+			});
+			m_body->setVelocity({ 0,0 });
+			break;
+		default:
+			break;
+		}
 	}
 
 	void Spider::onCollisionStay(std::shared_ptr<Jam::Domain::Physics::IPhysicsBody> other)
