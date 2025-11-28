81f85f38e829f98fbfc84c8caf28fe7bd7c1dc16
kinako6 <[21413007hk@yamato-univ.jp]>
2025-11-29 00:52:03 +0900

    変数名についてコメントを追加

--- Jam/src/Presentation/Scenes/Transition/IrisTransition.cpp
+++ Jam/src/Presentation/Scenes/Transition/IrisTransition.cpp
@@ -25,7 +25,6 @@ namespace Jam::Presentation::Scenes
 		const double tEase = EaseInOutExpo(t);
 		const double currentRadius = Math::Lerp(m_maxRadius, 0.0, tEase);
 
-		// drawFrame(innerThickness, outerThickness, color)
 		Circle{ m_center, currentRadius }
 		.drawFrame(0.0, m_maxRadius, m_color);
 	}
@@ -46,7 +45,7 @@ namespace Jam::Presentation::Scenes
 	{
 		m_center = center;
 		// 中心が変わった場合、最大半径が変わる可能性があるため再計算
-		// dist: Distance, TL: Top-Left, TR: Top-Right
+		// dist: Distance, TL: Top-Left, TR: Top-Right, BL: Bottom-Left, BR: Bottom-Right
 		double distTL = m_center.distanceFrom(Vec2{ 0, 0 });
 		double distTR = m_center.distanceFrom(Vec2{ m_area.x, 0 });
 		double distBL = m_center.distanceFrom(Vec2{ 0, m_area.y });
